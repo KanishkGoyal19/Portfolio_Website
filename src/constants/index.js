910d152763761d544e44bbb58a710a236287372a
Kanishk Goyal <goyalk@uoguelph.ca>
2025-05-08 02:18:29 -0400

    Updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,24 +1,24 @@
 export const experiences = [
   {
-    title: "Guest Registration and Door Staff",
-    company_name: "University of Guelph",
-    icon: "/Images/uofg.jpg", // Replace with the actual path or URL for the icon if needed
-    iconBg: "#FFF",
-    date: "August 2024 - Present",
-    points: [
-      "Enforced security protocols by conducting thorough ID checks to verify student status, ensuring compliance with Residence Life Services rules and maintaining a safe environment for students.",
-      "Managed incident reporting and promptly addressed safety concerns, reducing unauthorized entries by 20% and supporting a secure, orderly residence environment for over 100 guests daily."
-    ],
-  },
-  {
     title: "Summer Intern - Production Assistant",
     company_name: "Dawn Foods",
     icon: "/Images/dawnfoods.png", // Replace with the actual path or URL for the icon if needed
     iconBg: "#FFF",
-    date: "May 2024 - August 2024",
+    date: "Summer 2024, May 2025 - Present",
     points: [
       "Streamlined production processes by implementing rigorous quality control measures, reducing contamination risks,and enhancing allergen safety through proactive cleaning and maintenance, ensuring compliance with GMP and HACCP standards.",
       "Improved data tracking efficiency by logging production metrics and generating insightful reports for management, enabling more informed decision-making and optimizing production workflows."
+    ],
+  },
+  {
+    title: "Guest Registration and Door Staff",
+    company_name: "University of Guelph",
+    icon: "/Images/uofg.jpg", // Replace with the actual path or URL for the icon if needed
+    iconBg: "#FFF",
+    date: "August 2024 - April 2025",
+    points: [
+      "Enforced security protocols by conducting thorough ID checks to verify student status, ensuring compliance with Residence Life Services rules and maintaining a safe environment for students.",
+      "Managed incident reporting and promptly addressed safety concerns, reducing unauthorized entries by 20% and supporting a secure, orderly residence environment for over 100 guests daily."
     ],
   },
   {
@@ -43,11 +43,11 @@ export const projects = [
     source_code_link: "https://github.com/KanishkGoyal19/Chattr.git",
   },
   {
-    title: "Chef AI",
-    description: "A React-based web application that generates personalized recipes using the Claude AI API. It features a dynamic interface and a responsive design powered by Tailwind CSS and Shadcn, ensuring a seamless user experience across devices.",
-    tags: [{ name: "React", color: "text-blue-500" }, { name: "JavaScript", color: "text-yellow-500" }, { name: "API Integration", color: "text-orange-500" }, { name: "Tailwind CSS", color: "text-pink-500" },{ name: "Vercel", color: "text-purple-500" } ],
+    title: "ForkCast",
+    description: "Forkcast is a full-stack recipe generator that crafts personalized meal suggestions based on your available ingredients, dietary preferences, and ingredient restrictions. Powered by the Claude AI API and backed by MongoDB.",
+    tags: [{ name: "React", color: "text-blue-500" }, { name: "JavaScript", color: "text-yellow-500" }, { name: "API Integration", color: "text-orange-500" }, { name: "Backend Functionality", color: "text-pink-500" },{ name: "Vercel", color: "text-purple-500" } ],
     image: "/Images/Project1.png",
-    source_code_link: "https://github.com/KanishkGoyal19/Chef_AI.git",
+    source_code_link: "https://github.com/KanishkGoyal19/Fork-Cast",
   },
 ];
 
